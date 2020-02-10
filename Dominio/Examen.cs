c66a1a7f50c439e61444e12b5994bf6c1239ee1b
leohoet98@gmail.com <leohoet98@gmail.com>
2020-02-10 08:11:22 -0300

    Modificado id examen

--- Dominio/Examen.cs
+++ Dominio/Examen.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,8 @@ namespace Trabajo_Integrador.Dominio
 
 
         private int iRespuestasCorrectas;
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
 
 
